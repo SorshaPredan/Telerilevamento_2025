34560ca2b95223537e799196dee6aba3fc6bed00
SorshaPredan <sorsha.predan@gmail.com>
2025-03-06 15:04:27 +0100

    Update 02_visualization_.R

--- Code/02_visualization_.R
+++ Code/02_visualization_.R
@@ -5,7 +5,7 @@ library(devtools)
 install_github("ducciorocchini/imageRy")
 
 library(terra)
-libary(imageRy)
+library(imageRy)
 
 # Listing file
 im.list()
@@ -23,3 +23,45 @@ plot(b2, col=cl)
 # https://sites.stat.columbia.edu/tzheng/files/Rcolor.pdf
 cl <- colorRampPalette(c("darkorchid3", "chartreuse", "cyan"))(100)
 plot(b2, col=cl)
+
+# Band 3
+b3 <- im.import("sentinel.dolomites.b3.tif")
+# Band 4
+b4 <- im.import("sentinel.dolomites.b4.tif")
+# Band 8
+b8 <- im.import("sentinel.dolomites.b8.tif")
+
+par(mfrow=c(1,4))
+b2 <- im.import("sentinel.dolomites.b2.tif")
+b3 <- im.import("sentinel.dolomites.b3.tif")
+b4 <- im.import("sentinel.dolomites.b4.tif")
+b8 <- im.import("sentinel.dolomites.b8.tif")
+
+par(mfrow=c(1,4))
+plot(b2)
+plot(b3)
+plot(b4)
+plot(b8)
+
+dev.off()
+
+im.multiframe(1,4)
+plot(b2)
+plot(b3)
+plot(b4)
+plot(b8)
+
+
+# Exercise: plot the bands using im.miltiframe() ore on top of the other
+im.multiframe(4,1)
+plot(b2)
+plot(b3)
+plot(b4)
+plot(b8)
+
+im.multiframe(2,2)
+plot(b2)
+plot(b3)
+plot(b4)
+plot(b8)
+
