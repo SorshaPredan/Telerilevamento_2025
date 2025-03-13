20ffb79a0d6c8b731b3055e55c2eeb6bbe8bedc8
SorshaPredan <sorsha.predan@gmail.com>
2025-03-13 15:47:21 +0100

    Update 03_spectral_indices.R

--- Code/03_spectral_indices.R
+++ Code/03_spectral_indices.R
@@ -53,11 +53,11 @@ im.plotRGB(mato2006, r=3, g=2, b=1)
 # Exercise: plot only the first layer of mato2006
 dev.off()
 
-# non viene però utilizzato
+# Non viene PIù utilizzato
 plot(mato2006$matogrosso~6209_lrg_1)
-# invece si utilizzano le []
+# Invece si utilizzano le []
 plot(mato2006[[1]])
-# utilizzo di viridis
+# Utilizzo di viridis
 plot(mato2006[[1]], col=magma (100))
 plot(mato2006[[1]], col=mako (100))
 
@@ -68,10 +68,98 @@ plot(mato2006[[1]], col=mako (100))
 # Stressed tree: NIR = 100 (8 bit), red = 30 (8 bit)
 # DVI =          NIR - red = 100 - 30 = 70 (pianta sotto stress)
 
+# Calculating DVI
 
+library(imageRy)
+library(terra)
+library(viridis)
 
+im.list()
+mato1992 <- im.import("matogrosso_l5_1992219_lrg.jpg")
+mato1992 <- flip(mato1992)
+plot(mato1992)
 
+mato2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
+mato2006 <- flip(mato2006)
+plot(mato2006)
 
+im.multiframe(1,2)
+plot(mato1992)
+plot(mato2006)
+
+# 1 = NIR
+# 2 = red
+
+dvi1992 = mato1992[[1]] - mato1992[[2]] # NIR - red
+plot(dvi1992)
+
+# Range dvi
+# Maximum: NIR - red = 255 - 0 = 255
+# Minimum: NIR - red = 0 - 255 = -255
+
+plot(dvi1992, col=magma(100))
+plot(dvi1992, col=inferno(100))
+
+# Calculate dvi for 2006
+dvi2006 = mato2006[[1]] - mato2006[[2]] # NIR - red
+plot(dvi2006)
+
+# Range dvi
+# Maximum: NIR - red = 100 - 30 = 70
+# Minimum: NIR - red = 30 - 100 = -70
+
+plot(dvi2006, col=magma(100))
+plot(dvi2006, col=inferno(100))
+
+im.multiframe(1,2)
+plot(dvi1992, col=inferno(100))
+plot(dvi2006, col=inferno(100))
+
+# Different radiometric resolutions
+
+# DVI 8 bit: range (0-255)
+# Maximum: NIR - red = 255 - 0 = 255
+# Minimum: NIR - red = 0 - 255 = -255
+
+# DVI 4 bit: range (0-15)
+# Maximum: NIR - red = 15 - 0 = 15
+# Minimum: NIR - red = 0 - 15 = -15
+
+# NDVI (standardizzazione) 8 bit: range (0-255)
+# Maximum: (NIR - red) / (NIR + red) = (255 - 0) / (255 + 0) = 1
+# Minimum: NIR - red / (NIR + red) = (0 - 255) / (0 + 255) = -1
+
+# NDVI (standardizzazione) 4 bit: range (0-15)
+# Maximum: (NIR - red) / (NIR + red) = (15 - 0) / (15 + 0) = 1
+# Minimum: NIR - red / (NIR + red) = (0 - 15) / (0 + 15) = -1
+
+ndvi1992 = (mato1992[[1]] - mato1992[[2]]) / (mato1992[[1]] + mato1992[[2]])
+# ndvi1992 = dvi1992 / (mato1992[[1]] + mato1992[[2]])
+plot(ndvi1992)
+
+ndvi2006 = (mato2006[[1]] - mato2006[[2]]) / (mato2006[[1]] + mato2006[[2]])
+# ndvi2006 = dvi2006 / (mato2006[[1]] + mato2006[[2]])
+plot(ndvi2006)
+
+# Functions from imageRy
+dvi1992auto = im.dvi(mato1992, 1, 2)
+dev.off()
+plot(dvi1992auto)
+
+dvi2006auto = im.dvi(mato2006, 1, 2)
+plot(dvi2006auto)
+
+ndvi1992auto = im.ndvi(mato1992, 1, 2)
+dev.off()
+plot(ndvi1992auto)
+
+ndvi2006auto = im.ndvi(mato2006, 1, 2)
+dev.off()
+plot(ndvi2006auto)
+
+im.multiframe(1,2)
+plot(ndvi1992)
+plot(ndvi1992auto)
 
 
 
