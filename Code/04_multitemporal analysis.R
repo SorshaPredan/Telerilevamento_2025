f62caebc687140962d713b67a2774ac41e02378d
SorshaPredan <sorsha.predan@gmail.com>
2025-03-31 09:48:26 +0200

    Update 04_multitemporal analysis.R

--- Code/04_multitemporal analysis.R
+++ Code/04_multitemporal analysis.R
@@ -4,13 +4,16 @@
 library(imageRy)
 library(terra)
 library(viridis)
-# library(ggridges)
+library(ggplot2)
+# library(ggridges) for ridgeline plots
+# library(ggplot2)  for ridgeline plots
 
 # Listing the data
 im.list()
 
-EN_01 = im.import ("EN_01.png")
-EN_01 = flip(EN_01)
+# Importing data
+EN_01 <- im.import ("EN_01.png")
+EN_01 <- flip(EN_01)
 plot(EN_01)
 
 EN_13 = im.import("EN_13.png")
@@ -26,12 +29,12 @@ ENdif = EN_01[[1]] - EN_13[[1]]
 plot(ENdif)
 plot(ENdif, col=inferno(100))
 
-#------
 
-# Greenland ice met
-
+# Example 2: ice met in Greenland
 gr <- im.import("greenland")
 
+# Exercise: plot in a multiframe the first and last elements of gr
+par(mfrow=c(1,2))
 im.multiframe(1,2)
 plot(gr[[1]], col=rocket(100))
 plot(gr[[4]], col=rocket(100))
@@ -39,6 +42,8 @@ plot(gr[[4]], col=rocket(100))
 grdif <- gr[[4]] - gr[[1]] # 2015 - 2000
 plot(grdif)
 # All the yellow parts are those in which there is a higher value in 2015
+grdif <- gr[[1]] - gr[[4]] # 2000 - 2015
+plot(grdif)
 
 # Ridgeline plots
 im.ridgeline(gr, scale=1)
@@ -49,19 +54,35 @@ im.ridgeline(gr, scale=3, palette="inferno")
 im.list()
 
 # Exercise: import the NDVI data from Sentinel 
+# NDVI: phehnology
 ndvi <- im.import("Sentinel2_NDVI")
-im.ridgeline(ndvi, scale=2)
+im.plotRGB(ndvi, 1, 2, 3)
 
-# Changing names
-# sources     : Sentinel2_NDVI_2020-02-21.tif  
-#               Sentinel2_NDVI_2020-05-21.tif  
-#               Sentinel2_NDVI_2020-08-01.tif  
-#               Sentinel2_NDVI_2020-11-27.tif  
+im.ridgeline(ndvi, scale=2)
 
 names(ndvi) <- c("02_Feb", "05_May", "08_Aug", "11_Nov")
 im.ridgeline(ndvi, scale=2)
+im.ridgeline(ndvi, scale=2, palette="magma")
 im.ridgeline(ndvi, scale=2, palette="mako")
 
+# Exercise: compose a RGB image with the years of Greenland temperature
+im.plotRGB(gr, r=1, g=2, b=4)
+# gr: 2000, 2005, 2010, 2015
+
+# Ridgeline plots
+# Example with NDVI data
+
+# NDVI file
+ndvi <- im.import("NDVI_2020")
+
+plot(ndvi)
+
+plot(ndvi[[2]],ndvi[[3]])
+abline(0,1,col="red")
+
+
+
+# IN PIU NON ANCORA FATTO
 pairs(ndvi)
 
 plot(ndvi[[1]], ndvi[[2]])
