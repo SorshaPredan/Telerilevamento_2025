09d282b0c0df636bdac74df369b93b3450dce52a
SorshaPredan <sorsha.predan@gmail.com>
2025-03-31 09:59:03 +0200

    Update 04_multitemporal analysis.R

--- Code/04_multitemporal analysis.R
+++ Code/04_multitemporal analysis.R
@@ -16,8 +16,8 @@ EN_01 <- im.import ("EN_01.png")
 EN_01 <- flip(EN_01)
 plot(EN_01)
 
-EN_13 = im.import("EN_13.png")
-EN_13 = flip(EN_13)
+EN_13 <- im.import("EN_13.png")
+EN_13 <- flip(EN_13)
 plot(EN_13)
 
 # Exercise: plot the two images one beside the order
