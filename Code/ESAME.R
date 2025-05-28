e3a7f8d3a55912d733a462a72cb51ba4ae70907d
SorshaPredan <sorsha.predan@gmail.com>
2025-05-28 16:15:51 +0200

    Create _ESAME_.R

---
+++ Code/ESAME.R
@@ -0,0 +1,12 @@
+# Titolo del progetto
+# Scopo del progetto diviso in tre anni (2018 - 2021 - 2024)
+# pacchetti utilizzati (commento a cosa servono)
+
+
+# immagini True color + commento
+# immagini False color per il calcolo NDVI
+# immagini False color per il calcolo NDRE
+# NDVI cosa serve + commento risultato
+# NDRE cosa serve + commento risultato
+# Grafici di confronto 
+# Commento finale
