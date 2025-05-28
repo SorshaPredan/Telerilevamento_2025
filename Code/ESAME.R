b89667965cdf0b645504b5f51beb99e7a5a06eb5
SorshaPredan <sorsha.predan@gmail.com>
2025-05-28 16:35:24 +0200

    Update ESAME.R

--- Code/ESAME.R
+++ Code/ESAME.R
@@ -9,10 +9,41 @@ library(viridis)
 library(cblindplot)
 library(devtools) # Da rivedere a cosa serve
 
-
-# immagini True color + commento
+# Immagini True color + commento
 # immagini False color per il calcolo NDVI
 # immagini False color per il calcolo NDRE
+
+# Importo l'immagine "True color" (RGB) per l'ANNO 2018
+UD_2018 <-rast()
+UD_2018
+# Importo l'immagine "False color" (NIR, Red, Green) per l'ANNO 2018
+UDNIR_2018 <-rast()
+UDNIR_2018
+# Importo l'immagine "False color" (NIR, Red Edge, Blue) per l'ANNO 2018
+UDRedE_2018 <-rast()
+UDRedE_2018
+
+# Importo l'immagine "True color" (RGB) per l'ANNO 2021
+UD_2021 <-rast()
+UD_2021
+# Importo l'immagine "False color" (NIR, Red, Green) per l'ANNO 2021
+UDNIR_2021 <-rast()
+UDNIR_2021
+# Importo l'immagine "False color" (NIR, Red Edge, Blue) per l'ANNO 2021
+UDRedE_2021 <-rast()
+UDRedE_2021 
+
+# Importo l'immagine "True color" (RGB) per l'ANNO 2024
+UD_2024 <-rast()
+UD_2024
+# Importo l'immagine "False color" (NIR, Red, Green) per l'ANNO 2024
+UDNIR_2024 <-rast()
+UDNIR_2024
+# Importo l'immagine "False color" (NIR, Red Edge, Blue) per l'ANNO 2024
+UDRedE_2024 <-rast()
+UDRedE_2024 
+
+# Calcolo NDVI + calcolo NDRE
 # NDVI cosa serve + commento risultato
 # NDRE cosa serve + commento risultato
 # Grafici di confronto 
