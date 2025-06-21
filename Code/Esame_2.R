5c0aefe3bf85df62d8f827e4de3990407de6dfcb
SorshaPredan <sorsha.predan@gmail.com>
2025-06-21 17:47:50 +0200

    Update Esame_2.R

--- Code/Esame_2.R
+++ Code/Esame_2.R
@@ -80,6 +80,28 @@ Tr2018_bRE <-TarvisioRedE_2018[[2]]
 Tr2018_bb <-TarvisioRedE_2018[[3]]
 Tr2018_bg <-TarvisioNIR_2018[[3]]
 Tarvisio2018_RE <-c(Tr2018_bnir, Tr2018_bRE, Tr2018_bb, Tr2018_bg)
+# Anno 2022
+Tr2022_br <-Tarvisio_2022[[1]]
+Tr2022_bg <-Tarvisio_2022[[2]]
+Tr2022_bb <-Tarvisio_2022[[3]]
+Tr2022_nir <-TarvisioNIR_2022[[1]]
+Tarvisio2022 <-c(Tr2022_br, Tr2022_bg, Tr2022_bb, Tr2022_nir)
+# Bande Red Edge
+Tr2022_bnir <-TarvisioRedE_2022[[1]]
+Tr2022_bRE <-TarvisioRedE_2022[[2]]
+Tr2022_bb <-TarvisioRedE_2022[[3]]
+Tr2022_bg <-TarvisioNIR_2022[[3]]
+Tarvisio2022_RE <-c(Tr2022_bnir, Tr2022_bRE, Tr2022_bb, Tr2022_bg)
+
+
+# Calculate dvi for 2018
+dvi2018 = Tr2018_nir - Tr2018_br # NIR - red
+plot(dvi2018)
+dvi2022 = Tr2022_nir - Tr2022_br # NIR - red
+plot(dvi2022)
+im.multiframe(2,1)
+plot(dvi2018, col=inferno (100))
+plot(dvi2018, col=inferno (100))
 
 
 
